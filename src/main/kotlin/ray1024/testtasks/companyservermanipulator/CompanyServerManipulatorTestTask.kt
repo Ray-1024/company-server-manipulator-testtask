c4f0a5ba26a2235b87e48ca5c8be7c31dab88c92
Ray_1024 <skovorodnikovda@mail.ru>
2025-06-23 18:30:15 +0300

    Initial commit

---
+++ src/main/kotlin/ray1024/testtasks/companyservermanipulator/CompanyServerManipulatorTestTask.kt
@@ -0,0 +1,11 @@
+package ray1024.testtasks.companyservermanipulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanyServerManipulatorTestTask
+
+fun main(args: Array<String>) {
+    runApplication<CompanyServerManipulatorTestTask>(*args)
+}
---
+++ src/test/kotlin/ray1024/testtasks/companyservermanipulator/CompanyServerManipulatorTestTaskTests.kt
@@ -0,0 +1,13 @@
+package ray1024.testtasks.companyservermanipulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompanyServerManipulatorTestTaskTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
